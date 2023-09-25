73d6ce3b030a551051b32230ff18edd8b3737b2c
Antonio0512 <antonioboyanov9@gmail.com>
2023-09-25 19:01:22 +0300

    Adding the right bar

--- frontend/src/components/rightbar/Rightbar.js
+++ frontend/src/components/rightbar/Rightbar.js
@@ -3,7 +3,87 @@ import "./rightbar.css"
 export const Rightbar = () => {
     return (
         <div className="rightbar">
-            Rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="/assets/gift.png" alt=""/>
+                    <span
+                        className="birthdayText"><b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.</span>
+                </div>
+                <img className="rightbarAd" src="/assets/ad.png" alt=""/>
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg"
+                                 src="/assets/person/person-1.jpeg"
+                                 alt=""
+                            />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Doe</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg"
+                                 src="/assets/person/person-1.jpeg"
+                                 alt=""
+                            />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Doe</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg"
+                                 src="/assets/person/person-1.jpeg"
+                                 alt=""
+                            />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Doe</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg"
+                                 src="/assets/person/person-1.jpeg"
+                                 alt=""
+                            />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Doe</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg"
+                                 src="/assets/person/person-1.jpeg"
+                                 alt=""
+                            />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Doe</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg"
+                                 src="/assets/person/person-1.jpeg"
+                                 alt=""
+                            />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Doe</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg"
+                                 src="/assets/person/person-1.jpeg"
+                                 alt=""
+                            />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">John Doe</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
